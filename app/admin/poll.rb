251c1f2e42c4aeffcda9680812f584b1c1aa02f9
Prelang Builder <builder@prelang.com>
2015-05-16 06:51:01 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Poll' to allow for create, update

--- app/admin/poll.rb
+++ app/admin/poll.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Poll do
+  permit_params :question, :subtext
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
