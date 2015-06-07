59b2709ba11964e0fe90670440c516ce193b56c8
Brendon Body <bbody.project@gmail.com>
2015-06-07 17:42:51 +1000

    Changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+	def count_votes user
+		votes = 0
+		polls = Poll.where(user_id: user)
+		polls.each do |poll|
+			votes = votes + poll.votes
+		end
+		votes
+	end
 end
